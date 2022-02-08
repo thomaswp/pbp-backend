f16bfd1e788198ee1648d405ad68992281c48b39
Melody Griesen <jvgriese@ncsu.edu>
2022-02-07 21:09:21 -0500

    removed folder copy of pbpcode

--- pbpcode/public/templates/foreach.js
+++
@@ -1,9 +0,0 @@
-const array = $$array;
-// TODO: Begin loop
-    // TODO: Get the item and index of each item in the array
-    processItem(item, index);
-// End loop
-
-function processItem(item, index) {
-    console.log(`Item ${index} of list is ${item}`);
-}
--- pbpcode/src/rete-components/sockets.js
+++
@@ -1,8 +0,0 @@
-import { Socket } from "rete";
-
-export const numSocket = new Socket('Number value');
-export const listSocket = new Socket('List value');
-export const loopSocket = new Socket('Loop value');
-export const predicateSocket = new Socket('Predicate value');
-export const boolSocket = new Socket('Boolean value');
-export const stringSocket = new Socket('String value');
