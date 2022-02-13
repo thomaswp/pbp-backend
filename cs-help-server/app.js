960e808784c0a2516a341b2afeab9abb55fcd915
vaputra <vaputra@ncsu.edu>
2022-02-13 16:00:18 -0500

    Create authentication preparation code. Still need to have session management and main authentication.

--- cs-help-server/app.js
+++ cs-help-server/app.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.NODE_DOCKER_PORT || 5000
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.NODE_DOCKER_PORT || 5000;
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
+
+// Connect to database
+const db = require("./src/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
---
+++ cs-help-server/src/models/index.js
@@ -0,0 +1,14 @@
+const dbConfig = require("../config/db.config.js");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+
+// return all model instances
+db.users = require("./user.model.js")(mongoose);
+db.federatedidentity = require("./federated_identity.model.js")(mongoose);
+
+// export db
+module.exports = db;
