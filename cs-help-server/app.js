85bd2a5f919a153c9d8ea64fe40f2cad5bba73da
vaputra <vaputra@ncsu.edu>
2022-02-13 17:06:18 -0500

    Update the code to have a sample rest api that return a sample user.

--- cs-help-server/app.js
+++ cs-help-server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.NODE_DOCKER_PORT || 5000;
+require("./src/routes/user.routes")(app);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
