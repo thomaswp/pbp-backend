34f1c2154f3da109b7adc0430f0ff40d58fb9d7f
vaputra <vaputra@ncsu.edu>
2022-02-16 14:17:21 -0500

    Test manage session backend

--- cs-help-server/src/routes/user.routes.js
+++ cs-help-server/src/routes/user.routes.js
@@ -3,13 +3,16 @@
  * 
  */
 const express = require("express");
+const ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 let router = express.Router();
+let ensureLoggedIn = ensureLogIn();
 
-router.get('/api/v1/users/:id', (req, res) => {
+router.get('/api/v1/users/:id', ensureLoggedIn, (req, res) => {
     let id = req.params.id;
     let sampleUser = {
         name: "John Doe " + id,
         email: "jdoe@email.com",
+        session: req.session.passport.user
     }
     res.json(sampleUser);
 });
