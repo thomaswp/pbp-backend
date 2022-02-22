ab3524b6571380c219742d5cc1005ab1a6397126
vaputra <vaputra@ncsu.edu>
2022-02-22 15:53:57 -0500

    try to fix CI

--- cs-help-server/test/mongo-global-hook-plugin.js
+++ cs-help-server/test/mongo-global-hook-plugin.js
@@ -1,19 +1,22 @@
-const { GenericContainer } = require('testcontainers');
- 
+const { GenericContainer } = require("testcontainers");
+
 module.exports.mochaHooks = {
-    beforeAll: async function () {
-        this.timeout(60 * 10000);
- 
-        global.mongoContainer = await new GenericContainer("mongo", "latest")
-            .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
-            .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
-            .withExposedPorts(27017)
-            .start();
- 
-        global.mongoUrl = `mongodb://root:example@127.0.0.1:${global.mongoContainer.getMappedPort(27017)}`;
-    },
- 
-    afterAll: async function() {
-        await global.mongoContainer.stop();
-    }
-}
+  beforeAll: async function () {
+    this.timeout(60 * 10000);
+    console.log("test started");
+    global.mongoContainer = await new GenericContainer("mongo", "latest")
+      .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
+      .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
+      .withExposedPorts(27017)
+      .start();
+    console.log("DB created");
+    global.mongoUrl = `mongodb://root:example@127.0.0.1:${global.mongoContainer.getMappedPort(
+      27017
+    )}`;
+  },
+
+  afterAll: async function () {
+    await global.mongoContainer.stop();
+    console.log("Finish unit test");
+  },
+};
