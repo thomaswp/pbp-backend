cd6dde9aa129b6ad63a355cb7193b19a617e29df
vaputra <vaputra@ncsu.edu>
2022-02-23 12:13:18 -0500

    Use docker-compose instead of test-container

--- cs-help-server/test/mongo-global-hook-plugin.js
+++ cs-help-server/test/mongo-global-hook-plugin.js
@@ -4,19 +4,17 @@ module.exports.mochaHooks = {
   beforeAll: async function () {
     this.timeout(60 * 10000);
     console.log("test started");
-    global.mongoContainer = await new GenericContainer("mongo", "latest")
-      .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
-      .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
-      .withExposedPorts(27017)
-      .start();
-    console.log("DB created");
-    global.mongoUrl = `mongodb://root:example@127.0.0.1:${global.mongoContainer.getMappedPort(
-      27017
-    )}`;
+    // global.mongoContainer = await new GenericContainer("mongo", "latest")
+    //   .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
+    //   .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example")
+    //   .withExposedPorts(27017)
+    //   .start();
+    // console.log("DB created");
+    global.mongoUrl = `mongodb://root:123456@0.0.0.0:7017/cshelp_db?authSource=admin`;
   },
 
   afterAll: async function () {
-    await global.mongoContainer.stop();
     console.log("Finish unit test");
+    process.exit();
   },
 };
