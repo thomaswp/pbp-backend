3e1bde8da1e1c3d774967ab120bc13c18b079d09
jvgriese <jvgriese@ncsu.edu>
2022-03-09 11:46:46 -0500

    feat: add new reload() method to Mongoose models

--- cs-help-server/src/models/index.js
+++ cs-help-server/src/models/index.js
@@ -1,7 +1,22 @@
 const dbConfig = require("../config/db.config.js");
 const mongoose = require("mongoose");
 
+// configure mongoose to use standard JS promise
 mongoose.Promise = global.Promise;
+
+// add a new method on every Model called "reload" which refreshes from db
+// taken from:
+// https://github.com/Automattic/mongoose/issues/7781#issuecomment-929101197
+const reloadRecord = function reloadRecord(schema) {
+  schema.methods.reload = async function() {
+    const record = await this.constructor.findById(this); 
+    Object.assign(this, record);
+    return record;
+  }
+}
+mongoose.plugin(reloadRecord);
+
+
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
