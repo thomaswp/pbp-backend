f1515f507329461b0725020d6e1d93d4b074f602
vaputra <vaputra@ncsu.edu>
2022-03-23 14:32:31 -0400

    Create assignment routes and controllers

---
+++ cs-help-server/src/controllers/assignment.controller.js
@@ -0,0 +1,73 @@
+const db = require("../models");
+const { nanoid } = require("nanoid");
+const projectController = require("./project.controller");
+const Assignment = db.assignments;
+
+/**
+ *  This method creates a new assignment
+ * @param {*} assignment  the assignment data
+ */
+exports.createAssignment = async (assignment) => {
+  // error check - must have project name
+  if (!assignment.name) {
+    return false;
+  }
+  // create and save new project
+  const newAssignment = new Assignment({
+    _id: nanoid(),
+    name: assignment.name,
+    data: assignment.data,
+    copies: {},
+  });
+  await newAssignment.save();
+  // return the new project
+  return newAssignment;
+};
+
+/**
+ * Get assignment by id
+ * @param {*} assignmentID assignment ID
+ * @returns assignment that matches with the ID
+ */
+exports.getAssignment = async (assignmentID) => {
+  return await Assignment.findById(assignmentID);
+};
+
+/**
+ * Open assignment adn do checking of what to do
+ * CREATE: If the assignment does not have user ID and project associated with it
+ * OPEN: If the assignment does have user ID and project associated with it
+ * UNARCHIVE: If the assignment does have user ID and project associated with it but it is archived
+ * @param {*} assignmentID assignment ID
+ * @param {*} userID user ID
+ * return assignment ID
+ */
+exports.openAssignment = async (assignment, user) => {
+  // Check if userID is in the list of copies
+  if (assignment.copies[user._id]) {
+    let currentProject = await projectController.getProject(
+      assignment.copies[user._id]
+    );
+    currentProject = await projectController.setArchived(currentProject, false);
+    return assignment.copies[user._id];
+  }
+
+  // Create new project from assignment
+  const newProject = {
+    name: assignment.name,
+    data: assignment.data,
+    owner: user._id,
+  };
+  const createdProject = await projectController.createProject(
+    newProject,
+    user
+  );
+  if (!createdProject) {
+    return false;
+  }
+  // Add project to assignment copies
+  assignment.copies[user._id] = createdProject._id;
+  assignment.markModified("copies");
+  await assignment.save();
+  return createdProject._id;
+};
--- cs-help-server/src/routes/assignment.routes.js
+++ cs-help-server/src/routes/assignment.routes.js
@@ -1,95 +1,51 @@
 /**
- * Router to handle Project and Assignment request
+ * Router to handle Assignment request
  *
  */
- const express = require("express");
- let router = express.Router();
- const userController = require("../controllers/user.controller");
- const projectController = require("../controllers/project.controller");
- const isLoggedIn = require("../middleware/ensureLoggedIn");
- 
- /**
-  * API used to create new assignment
-  * url: POST /api/v1/assignment
-  * returns: newly created project
-  */
- router.post("/api/v1/assignment", isLoggedIn, async (req, res) => {
-   let projname = req.body.name;
-   let currentUser = await userController.findUser(req.session.passport?.user);
-   const project = {
-     name: projname,
-     owner: currentUser._id,
-   };
-   let returnedProject = await projectController.createProject(
-     project,
-     currentUser
-   );
-   console.log(returnedProject);
-   res.status(201);
-   res.json(returnedProject);
- });
- 
- /**
-  * API used to update assignment name
-  * url: PUT /api/v1/assignment/:id/name
-  * returns: newly updated project
-  */
- router.put("/api/v1/assignment/:id/name", isLoggedIn, async (req, res) => {
-   let projectid = req.params.id;
-   let project = await projectController.getProject(projectid);
-   if (project.owner !== req.session.passport?.user) {
-     res.status(401);
-     res.json({ errMsg: "Cannot edit project you do not own" });
-   } else if(req.body.name === "" ) {
-     res.status(500);
-     res.json({ errMsg: "Name cannot be empty" });
-   } else {
-     //updating project name in projects
-     // project.name = req.body.name;
-     const newName = req.body.name;
-     let updatedProject = await projectController.renameProject(
-       project,
-       newName
-     );
-     res.status(200);
-     res.json(updatedProject);
-   }
- });
- 
- /**
-  * API used to get an assignment by id
-  * url: GET /api/v1/projects/:id
-  * returns: project with matching id
-  */
- router.get("/api/v1/assignment/:id", isLoggedIn, async (req, res) => {
-   let id = req.params.id;
-   let project = await projectController.getProject(id);
-   if (!project) {
-     res.status(404);
-     res.json({ errMsg: "Project Not Found" });
-   } else {
-     res.status(200);
-     res.json(project);
-   }
- });
- 
- /**
-  * API used to update an assignment by id
-  * url: PUT /api/v1/assignment/:id/data
-  * returns: newly updated project
-  */
- router.put("/api/v1/assignment/:id/data", isLoggedIn, async (req, res) => {
-   const data = req.body.data;
-   let id = req.params.id;
-   let project = await projectController.getProject(id);
-   if (project.owner !== req.session.passport?.user) {
-     res.status(401);
-     res.json({ errMsg: "Cannot save a project you do not own" });
-   } else {
-     await projectController.saveProject(project, data);
-     res.status(200).send();
-   }
- });
- 
- module.exports = router;
- 
+const express = require("express");
+let router = express.Router();
+const userController = require("../controllers/user.controller");
+const projectController = require("../controllers/project.controller");
+const assignmentController = require("../controllers/assignment.controller");
+const isLoggedIn = require("../middleware/ensureLoggedIn");
+
+/**
+ * API used to create/open/unarchive new project from assignment
+ * CREATE: If the assignment does not have user ID and project associated with it
+ * OPEN: If the assignment does have user ID and project associated with it
+ * UNARCHIVE: If the assignment does have user ID and project associated with it but it is archived
+ * url: POST /api/v1/assignment
+ * returns: newly created project
+ */
+router.post("/api/v1/assignment", isLoggedIn, async (req, res) => {
+  let currentAssignment = await assignmentController.getAssignment(
+    req.body.assignmentID
+  );
+  let currentUser = await userController.findUser(req.session.passport?.user);
+
+  const projectID = assignmentController.openAssignment(
+    currentAssignment,
+    currentUser
+  );
+  if (!projectID) {
+    res
+      .status(400)
+      .json({ errMsg: "Failed to open the project from assignment." });
+  }
+  res.status(201);
+  res.json({ projectID: projectID });
+});
+
+/**
+ * Development API to populate assignment schema with examples
+ */
+router.post("/api/v1/insertassignments", isLoggedIn, async (req, res) => {
+  let createdAssignment = await assignmentController.createAssignment({name: req.body.name, data: req.body.data});
+  if(!createdAssignment) {
+    res
+      .status(400)
+      .json({ errMsg: "Failed to create an assignment" });
+  }
+  res.status(201);
+  res.json(createdAssignment);
+});
