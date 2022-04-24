04b7dbd6307513dd3f2ea1d9e0be53b6318d7f69
Melody Griesen <jvgriese@ncsu.edu>
2022-04-24 13:59:38 -0400

    log: change to 24h; ignore redirect route for security

--- cs-help-server/src/config/logger.config.js
+++ cs-help-server/src/config/logger.config.js
@@ -15,7 +15,7 @@ const logger = winston.createLogger({
   format: combine(
     colorize({ all: true }),
     timestamp({
-      format: "YYYY-MM-DD hh:mm:ss.SSS A",
+      format: "YYYY-MM-DD HH:mm:ss.SSS",
     }),
     align(),
     printf((info) => `[${info.timestamp}] ${info.level}: ${info.message.trim()}`)
@@ -35,16 +35,24 @@ module.exports = {
       format: combine(
         colorize({ all: true }),
         timestamp({
-          format: "YYYY-MM-DD hh:mm:ss.SSS A",
+          format: "YYYY-MM-DD HH:mm:ss.SSS",
         }),
         align(),
         printf((info) => `[${info.timestamp}] ${info.level}: ${info.message.trim()}`)
       ),
       meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      msg: "{{req.method}} {{req.url}}: {{res.statusCode}} ({{res.responseTime}}ms)", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      msg: "{{req.method}} {{req.path}}: {{res.statusCode}} ({{res.responseTime}}ms)", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
       // expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
       colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+      // skip some log messages based on request and/or response
+      // in this case, we don't want to log the redirect codes from oauth2
+      ignoreRoute: function (req, res) { 
+        // if (req.path.includes("/oauth2/redirect")) {
+        //   return true;
+        // } else {
+          return false;
+        // }
+       } 
     }));
 
 
@@ -58,7 +66,7 @@ module.exports = {
       format: combine(
         colorize({ all: true }),
         timestamp({
-          format: "YYYY-MM-DD hh:mm:ss.SSS A",
+          format: "YYYY-MM-DD HH:mm:ss.SSS",
         }),
         align(),
         printf((info) => `<AUTO> [${info.timestamp}] ${info.level}: ${info.message.trim()}`)
